492a8ea69a1f4102f53ef8693a5e3df3829264ab
Somrithy Nun <nunsomrithy098@gmail.com>
2025-02-19 14:41:49 +0700

    Fix Card with mapper and connect to Eureka

--- card/src/main/java/com/rithy/banking/card/controller/CardController.java
+++ card/src/main/java/com/rithy/banking/card/controller/CardController.java
@@ -15,24 +15,13 @@ import org.springframework.web.bind.annotation.*;
 public class CardController {
 
     private final CardService cardService;
-
-    private final CardMapper cardMapper;  // Ensure this is being injected properly
+    private final CardMapper cardMapper;
 
     @PostMapping
     public ResponseEntity<?> save(@RequestBody CardDTO dto){
         Card card = cardService.save(cardMapper.toCard(dto));
         return ResponseEntity.status(HttpStatus.CREATED).body(card);
     }
-
-//    @PostMapping
-//    public ResponseEntity<?> save(@RequestBody CardDTO dto){
-//        System.out.println("Received DTO: " + dto);  // Debugging
-//        Card card = cardMapper.toCard(dto);
-//        System.out.println("Mapped Entity: " + card); // Debugging
-//        card = cardService.save(card);
-//        System.out.println("Saved Entity: " + card); // Debugging
-//        return ResponseEntity.status(HttpStatus.CREATED).body(card);
-//    }
 
     @GetMapping
     public  ResponseEntity<?> list(){
--- card/src/main/java/com/rithy/banking/card/mapper/CardMapper.java
+++ card/src/main/java/com/rithy/banking/card/mapper/CardMapper.java
@@ -1,9 +1,9 @@
 package com.rithy.banking.card.mapper;
 
+
 import com.rithy.banking.card.dto.CardDTO;
 import com.rithy.banking.card.entity.Card;
 import org.mapstruct.Mapper;
-
 
 @Mapper(componentModel = "spring")
 public interface CardMapper {
